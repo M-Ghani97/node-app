fb09ff749619a0d3953e403618ee1e86ebec12e3
Usman Ghani <47071139+M-Ghani97@users.noreply.github.com>
2021-12-19 14:25:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world from Node.js app!!!')
+    res.send('Node.js app!!!')
 })
 app.listen(3000, () => {
     console.log('Server is up on 3000')
